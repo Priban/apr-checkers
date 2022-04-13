2c11484cda298638349cb797265f4eae8e9d9d22
Pribis <lukaspriban01@gmail.com>
2022-04-13 19:59:33 +0200

    grid draw

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print("funguju")
+from grid import Grid
 
-print("sachy jsou lepsi")
+grid = Grid(8)
+grid.draw([
+    {"location": (1, 1), "color": 0},
+    {"location": (6, 0), "color": 1},
+    {"location": (5, 4), "color": 0},
+    {"location": (7, 7), "color": 1},
+    {"location": (1, 7), "color": 0}
+])
