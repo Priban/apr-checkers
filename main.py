d62bd05b9d3bdee120819899e74dd95e4e59e4b0
Pribis <lukaspriban01@gmail.com>
2022-04-13 20:26:00 +0200

    first conflict resolved

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from grid import Grid
-<<<<<<< HEAD
 from game import Game
-
-=======
 from figure import Figure
 from rock import Rock
 from queen import Queen
 
 rock_1 = Rock(6, 0, 0)
 rock_2 = Rock(1, 1, 1)
->>>>>>> 814d08966afc37ba858ba403ef6bd8d8803e3300
 
 grid = Grid(8)
 grid.draw([
