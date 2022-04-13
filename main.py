814d08966afc37ba858ba403ef6bd8d8803e3300
Bumross <bumr007@gmail.com>
2022-04-13 20:18:03 +0200

    grid figures

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from grid import Grid
+from figure import Figure
+from rock import Rock
+from queen import Queen
+
+rock_1 = Rock(6, 0, 0)
+rock_2 = Rock(1, 1, 1)
 
 grid = Grid(8)
 grid.draw([
-    {"location": (1, 1), "color": 0},
-    {"location": (6, 0), "color": 1},
-    {"location": (5, 4), "color": 0},
-    {"location": (7, 7), "color": 1},
-    {"location": (1, 7), "color": 0}
+    {rock_1.give_information()}
 ])
