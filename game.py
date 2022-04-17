750b59bd6011024a4ddc1e859822c4f6323ecfea
Pribis <lukaspriban01@gmail.com>
2022-04-17 23:24:43 +0200

    move and move_is_valid abstract methods

--- game.py
+++ game.py
@@ -1,10 +1,12 @@
 
-from turtle import update
-
+from graphics import Graphics
+from rock import Rock
 
 class Game():
 
     def __init__(self):
+        self._grid = [[None for j in range(8)] for i in range(8)]
+        self.fill_in_figures()
         self._game_over = False
 
     def start(self):
@@ -17,4 +19,15 @@ class Game():
         input("teď hráč zadá pohyb: ")
            
     def draw(self):
-        print("teď se hra překreslí")
+        Graphics.draw(self._grid)
+
+    def fill_in_figures(self):
+      for i in range(3):
+        for j in range(8):
+          if (i + j) % 2 == 0:
+            self._grid[i][j] = Rock(i, j, 0)
+
+      for i in [5, 6, 7]:
+        for j in range(8):
+          if (i + j) % 2 == 0:
+            self._grid[i][j] = Rock(i, j, 1)
