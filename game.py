04060d3208550af7224ed77dad68cb1311dce58c
Pribis <lukaspriban01@gmail.com>
2022-04-18 16:39:05 +0200

    figure highlighting

--- game.py
+++ game.py
@@ -3,33 +3,56 @@ from graphics import Graphics
 from rock import Rock
 from queen import Queen
 
+
 class Game():
 
     def __init__(self):
         self._grid = [[None for j in range(8)] for i in range(8)]
         self.fill_in_figures()
         self._game_over = False
+        self._highlighted = None
 
     def start(self):
         while not self._game_over:
             self.update()
             self.draw()
-            
+
     def update(self):
         print("update")
-        input("teď hráč zadá pohyb: ")
-           
+        self.require_player_to_highlight_figure()
+        self.draw()
+
+    def require_player_to_highlight_figure(self):
+        while True:
+            coords = input("označ figurku: ").split(" ")
+
+            try:
+                (x, y) = list(map(lambda c: int(c), coords))
+            except ValueError:
+                print("špatný formát souřadnic")
+                continue
+
+            if x > 8 or y > 8 or x < 1 or y < 1:
+                print("souřadnice jsou od 1 do 8")
+                continue
+
+            if self._grid[x - 1][y - 1] == None:
+                print("na tomto políčku není žádná figurka")
+                continue
+
+            self._highlighted = self._grid[x - 1][y - 1]
+            return
+
     def draw(self):
-        Graphics.draw(self._grid)
+        Graphics.draw(self._grid, self._highlighted)
 
     def fill_in_figures(self):
-      for i in range(3):
-        for j in range(8):
-          if (i + j) % 2 == 0:
-            self._grid[i][j] = Rock(i, j, 0)
+        for i in range(3):
+            for j in range(8):
+                if (i + j) % 2 == 0:
+                    self._grid[i][j] = Rock(i, j, 0)
 
-      for i in [5, 6, 7]:
-        for j in range(8):
-          if (i + j) % 2 == 0:
-            self._grid[i][j] = Rock(i, j, 1)
-      self._grid[4][1] = Queen(i, j, 1)
+        for i in [5, 6, 7]:
+            for j in range(8):
+                if (i + j) % 2 == 0:
+                    self._grid[i][j] = Rock(i, j, 1)
--- graphics.py
+++ graphics.py
@@ -1,30 +1,37 @@
-from re import S
+from termcolor import colored
 from rock import Rock
 from queen import Queen
 
+
 class Graphics:
 
-  ROCK_TYPE_A = "x"
-  ROCK_TYPE_B = "o"
-  QUEEN_TYPE_A = "X"
-  QUEEN_TYPE_B = "O"
+    ROCK_TYPE_A = "x"
+    ROCK_TYPE_B = "o"
+    QUEEN_TYPE_A = "X"
+    QUEEN_TYPE_B = "O"
 
-  @classmethod
-  def draw(self, grid):
-    print("    1 2 3 4 5 6 7 8")
-    print("  -------------------")
+    @classmethod
+    def draw(self, grid, highlighted):
+        print("    1 2 3 4 5 6 7 8")
+        print("  -------------------")
 
-    for i in range(len(grid)):
-      radek = str(i + 1) + " |"
+        for i in range(len(grid)):
+            radek = str(i + 1) + " |"
 
-      for j in range(len(grid)):
-        radek += " "
-        if isinstance(grid[i][j], Rock):
-          radek +=  self.ROCK_TYPE_A if grid[i][j].color else self.ROCK_TYPE_B
-        elif isinstance(grid[i][j], Queen):
-          radek +=  self.QUEEN_TYPE_A if grid[i][j].color else self.QUEEN_TYPE_B
-        else:
-          radek += " "
+            for j in range(len(grid)):
+                radek += " "
+                if isinstance(grid[i][j], Rock):
+                    radek += colored(
+                        self.ROCK_TYPE_A if grid[i][j].color else self.ROCK_TYPE_B,
+                        "green" if grid[i][j] == highlighted else "white"
+                    )
+                elif isinstance(grid[i][j], Queen):
+                    radek += colored(
+                        self.QUEEN_TYPE_A if grid[i][j].color else self.QUEEN_TYPE_B,
+                        "green" if grid[i][j] == highlighted else "white"
+                    )
+                else:
+                    radek += " "
 
-      print(radek + " |")
-    print("  -------------------")
+            print(radek + " |")
+        print("  -------------------")
