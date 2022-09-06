b426e60c514f93227d58f5a916e99ed0893248e2
Bumross <bumr007@gmail.com>
2022-09-06 12:34:03 +0200

    deleting jumped figure

--- figure.py
+++ figure.py
@@ -11,6 +11,8 @@ class Figure(ABC):
         board[previous_x][previous_y] = None
         board[x][y] = self
         print("ahoj")
+        self.delete_jumped_figures(x, y, previous_x, previous_y, board)
+
 
     @abstractmethod
     def move_is_valid(self, position, current_position, board):
@@ -24,4 +26,13 @@ class Figure(ABC):
     def get_color(self):
         return self._color
 
-        
+    # projedeme diagonálu tahu a smažeme figurku, která byly na ní
+    def delete_jumped_figures(self, x, y, previous_x, previous_y, board):
+        (dir_x, dir_y) = (int((x-previous_x)/abs(x-previous_x)), int((y-previous_y)/abs(y-previous_y)))
+        position_x = previous_x + dir_x
+        position_y = previous_y + dir_y
+        while position_x != x:
+            if board[position_x][position_y]:
+                board[position_x][position_y] = None
+            position_x += dir_x
+            position_y += dir_y
